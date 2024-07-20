e77ca821f3a738a9c2c95adf573856cb48e82c65
Bolun Dai <daibolun19950216@gmail.com>
2024-07-20 13:38:39 +0000

    updated scripts to use hydra

--- scripts/sim.py
+++ scripts/sim.py
@@ -1,13 +1,13 @@
-import argparse
 import pickle
 import signal
 import sys
 import time
 
+import hydra
 import numpy as np
 from Go2Py.sim.mujoco import Go2Sim
+from omegaconf import DictConfig, OmegaConf
 from PIL import Image
-
 from sailing_through_pcds.mariner import Mariner
 from sailing_through_pcds.quadruped_controller import QuadrupedController
 from sailing_through_pcds.utils import (
@@ -17,28 +17,30 @@ from sailing_through_pcds.utils import (
 )
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--id", type=str, default="00")
-    parser.add_argument(
-        "--mapdir",
-        type=str,
-        default="/workspace/sailing-through-pcds-devcontainer/data/",
-    )
-    args = parser.parse_args()
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig):
+    map_dir = cfg["map_dir"]
+    map_id = cfg["map_id"]
 
-    map_name = f"map0{args.id}"
+    map_name = f"map0{map_id}"
     robot = Go2Sim(mode="highlevel", xml_path="../assets/go2.xml")
-    image = Image.open(f"{args.mapdir}{map_name}.png").convert("L")
+    image = Image.open(f"{map_dir}{map_name}.png").convert("L")
     image_np = np.array(image).reshape(-1) / 255
     robot.model.hfield_data = 2.0 * image_np
     robot.viewer.update_hfield(0)
-    robot.pos0[0:2] = np.array([-4.0, -4.0])
+    robot.pos0[0:2] = np.array(cfg["robot_start_pos"][:2])
     robot.reset()
 
-    p_target_world = np.array([6.0, 6.0, 0.0])
-    controller = QuadrupedController(a=0.5, b=0.3, c=0.2)
-    preview_controller = Mariner(a=0.8, b=0.1, c=0.2, order=4)
+    p_target_world = np.array(cfg["robot_goal_pos"])
+    controller = QuadrupedController(
+        a=cfg["vessel_a"], b=cfg["vessel_b"], c=cfg["vessel_c"]
+    )
+    preview_controller = Mariner(
+        a=cfg["mariner_a"],
+        b=cfg["mariner_b"],
+        c=cfg["mariner_c"],
+        order=cfg["mariner_d"],
+    )
 
     # Define the signal handler function
     def signal_handler(sig, frame):
@@ -50,7 +52,7 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
 
     # JIT run preview_target_body
-    lidar_points = robot.getLaserScan(max_range=10.0)["pcd"]
+    lidar_points = robot.getLaserScan(max_range=cfg["lidar_max_range"])["pcd"]
     controller.jit_run(lidar_points)
     preview_controller.jit_run(lidar_points, p_target_world)
 
@@ -58,7 +60,7 @@ def main():
     done = False
     success = False
 
-    for i in range(15000):
+    for i in range(cfg["num_iters"]):
         if done:
             break
         p_robot_world, _quat_robot_world = robot.getPose()
@@ -73,8 +75,8 @@ def main():
 
         p_target_body = world2body(p_target_world, p_robot_world, quat_robot_world)
 
-        if i % 100 == 0:
-            lidar_points = robot.getLaserScan(max_range=10.0)["pcd"]
+        if i % cfg["safe_vel_update_freq"] == 0:
+            lidar_points = robot.getLaserScan(max_range=cfg["lidar_max_range"])["pcd"]
             cbf, dcbf = controller.get_cbf(lidar_points)
             if preview_arm_counter == 0:
                 (
@@ -84,35 +86,45 @@ def main():
                     chosen_scale,
                 ) = preview_controller.controller(lidar_points, p_target_body)
 
-            preview_arm_counter = (preview_arm_counter + 1) % 4
+            preview_arm_counter = (preview_arm_counter + 1) % cfg["mariner_update_freq"]
 
             if np.isnan(cbf):
                 print("NaNs in CBF")
                 break
 
+            yaw_angle_in_body_frame = 0.0
             v_des, done = get_performance_controller(
-                np.zeros(2), preview_target_body[:2], 0.0, kv=1.0, kw=1.5, v_max=1.0
+                np.zeros(2),
+                preview_target_body[:2],
+                yaw_angle_in_body_frame,
+                kv=cfg["performance_controller_kv"],
+                kw=cfg["performance_controller_kw"],
+                v_max=cfg["performance_controller_v_max"],
             )
-            v_cmd = clip_velocity(v_des[:2], 1.0)
-            w_cmd = np.clip(v_des[2], -2.0, 2.0)
+            v_cmd = clip_velocity(v_des[:2], cfg["performance_controller_v_max"])
+            w_cmd = np.clip(
+                v_des[2],
+                -cfg["performance_controller_w_max"],
+                cfg["performance_controller_w_max"],
+            )
             vel = np.array([v_cmd[0], v_cmd[1], w_cmd])
             safe_vel = controller.get_control(vel, cbf, dcbf)
 
-        if i <= 20:
+        if i <= cfg["start_delay"]:
             robot.step(0.0, 0.0, 0.0)
         else:
             robot.step(
                 safe_vel[0],
                 safe_vel[1],
                 safe_vel[2],
-                kp=[0, 0.0, 0.0],
+                kp=[0.0, 0.0, 0.0],
                 ki=[0.0, 0.0, 0.0],
             )
 
         error = np.linalg.norm(
             np.array([robot.data.qpos[0], robot.data.qpos[1]]) - p_target_world[:2]
         )
-        if error < 0.5:
+        if error < cfg["success_distance"]:
             success = True
             break
 
